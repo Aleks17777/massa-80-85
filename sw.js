fcde5e55124eaefa474e8df05167d8f75b45bcd2
Aleks17777 <alexandr.webbox@gmail.com>
2026-09-22 19:51:46 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "massa-80-85-v1";
+const CACHE = "massa-80-85-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
